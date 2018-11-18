19a4687cd4cddc140df9f3e4248f2cf5f7ee6bbc
hamzagillani <hamza.gillani10@gmail.com>
2018-11-19 00:35:25 +0500

    Initial commit

---
+++ app/src/main/java/com/digicon_valley/explicitandimplicitintent/MainActivity.java
@@ -0,0 +1,43 @@
+package com.digicon_valley.explicitandimplicitintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button,button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button1);
+        button2=findViewById(R.id.button_2);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    button2.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            Intent intent=new Intent(MainActivity.this,Main3Activity.class);
+            startActivity(intent);
+
+        }
+    });
+    }
+
+
+
+
+
+
+}
